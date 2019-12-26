a08e827374f5e1e6ead10a21de6dcb34244398f9
MattD <forwardprefekt@users.noreply.github.com>
2019-12-26 17:34:29 -0600

    Create dl.ps1

---
+++ dl.ps1
@@ -0,0 +1,6 @@
+$2012r2 = 'http://care.dlservice.microsoft.com/dl/download/6/2/A/62A76ABB-9990-4EFC-A4FE-C7D698DAEB96/9600.17050.WINBLUE_REFRESH.140317-1640_X64FRE_SERVER_EVAL_EN-US-IR3_SSS_X64FREE_EN-US_DV9.ISO'
+$2016standard = 'http://care.dlservice.microsoft.com/dl/download/1/6/F/16FA20E6-4662-482A-920B-1A45CF5AAE3C/14393.0.160715-1616.RS1_RELEASE_SERVER_EVAL_X64FRE_EN-US.ISO'
+$win10enterprise = 'http://download.microsoft.com/download/1/B/F/1BFE5194-5951-452C-B62C-B2F667F9B86D/14393.0.160715-1616.RS1_RELEASE_CLIENTENTERPRISE_S_EVAL_X64FRE_EN-US.ISO'
+Start-BitsTransfer $2012r2core
+Start-BitsTransfer $2016standard
+Start-BitsTransfer $win10enterprise
